6b6fe51f43f3d58d8036fab200da9ad2ebed26f3
Maria Laura Staffini <mls54@st-andrews.ac.uk>
2016-11-07 14:19:59 +0000

    Non-working version using class

--- assign2/planetinfo.cc
+++ assign2/planetinfo.cc
@@ -1,12 +1,15 @@
+#include <string>
 #include "planetinfo.h"
 
+using namespace std;
+
 PlanetInfo::PlanetInfo() {
-  m_name=' ';
+  m_name=" ";
   m_dist=0;
   m_mass=0;
 }
 
-PlanetInfo::PlanetInfo(char name, double mass, double dist) {
+PlanetInfo::PlanetInfo(string name, double mass, double dist) {
   m_name=name;
   m_dist=dist;
   m_mass=mass;
@@ -14,9 +17,9 @@ PlanetInfo::PlanetInfo(char name, double mass, double dist) {
 
 double PlanetInfo::mass() {return m_mass;}
 double PlanetInfo::dist() {return m_dist;}
-char PlanetInfo::name() {return m_name;}
+string PlanetInfo::name() {return m_name;}
 
-void PlanetInfo::setName(char name) {m_name=name;}
+void PlanetInfo::setName(string name) {m_name=name;}
 void PlanetInfo::setMass(double mass) {m_mass=mass;}
 void PlanetInfo::setDistance(double dist) {m_dist=dist;}
 
--- assign2/planetinfo.h
+++ assign2/planetinfo.h
@@ -1,13 +1,18 @@
 #ifndef PLANETINFO_H
 #define PLANETINFO_H
 
+#include <string>
+
+using namespace std;
+
 class PlanetInfo {
 
  public:
-  
-  PlanetInfo(char name, double mass, double dist);
+
+  PlanetInfo();
+  PlanetInfo(string name, double mass, double dist);
   // getters
-  char name();
+  string name();
   double mass();
   double dist();
   // setters
@@ -17,9 +22,9 @@ class PlanetInfo {
 
  private:
   
-  char m_name;
+  string m_name;
   double m_mass;
   double m_dist;
-}
+};
 
 #endif
