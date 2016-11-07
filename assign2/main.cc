586e808daa80cf2295535b099e9eb38921a92d13
Maria Laura Staffini <mls54@st-andrews.ac.uk>
2016-11-07 10:11:50 +0000

    Trying to troubleshoot making

--- assign2/main.cc
+++ assign2/main.cc
@@ -1,9 +1,13 @@
 #include <cmath>
 #include <iostream>
 #include <string>
-#include <utilities.h>
+#include <vector>
+#include <algorithm>
+#include <planetinfo.h>
 
-using namespace std; 
+using namespace std;
+
+void checkExtant(ifstream&);
 
 main {
 
@@ -16,37 +20,43 @@ main {
   cin >> filename;
   
   // create the name of the output file for later:
-  string outfile;
-  string dummy1;
-  dummy1=filename;
-  string dummy2="out";
-  outfile=dummy2+dummy1.erase(0,2);
+  string outhandle="output";
+  string outfile_name=outhandle+"_name_ordered.txt";
+  string outfile_mass=outhandle+"_mass_ordered.txt";
+  string outfile_distance=outhandle+"_distance_ordered.txt";
   
   ifstream myInput;
-  ofstream myOutput;
+  //ofstream myOutput;
+
+  const int numberOfPlanets=9;
 
   // Open input and output files for read/write:
   myInput.open( filename.c_str() );
-  myOutput.open( outfile.c_str() );
+  // myOutput.open( outfile.c_str() );
 
   checkExtant(myInput);
-  
-  cout << countRows
-  //PlanetInfo data[len];
 
+  typedef vector<PlanetInfo> planetVector;
+  planetVector data(numberOfPlanets);
+
+  for (i=0; i<numberOfPlanets; ++i) {
+    myInput >> planetVector[i].name >> planetVector[i].mass >> planetVector[i].distance;
+    cout << "Reading " << planetVector[i] <<endl;
+  }
+
+  myInput.close();
+  
 }
 
 //////////////////////////////////////////////////////////////////
 
-int countRows(ifstream &mystream) {
-  int rows=0;
-  int n;
- 
-  rows=count(istreambuf_iterator<char>(mystream), 
-  istreambuf_iterator<char>(), '\n');
-  cout << rows << " rows in file. \n";
-  return rows;
+void checkExtant(ifstream &mystream) {
+  if ( myInput.fail() ) {
+    cout << "File does not exist. \n";
+    exit(1); 
+  }
+  if (!myInput.is_open() ) {
+    cout << "File failed to open. \n";
+    exit(1);
+  }
 }
-
-// Check extant
-
